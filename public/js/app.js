fa3db4a75b6cf496ee5fbb380a5aa06b40c68bcd
raoul2000 <manu34@free.fr>
2019-02-05 22:57:23 +0100

    import document and update taglist

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,9 @@
 // @ts-nocheck
+const NEW_TAG_PREFIX = 'tmd_tagId:';
 
 const renderTags = (tags) => tags.map((t) => `<span class="label label-primary">${t.name}</span> `);
+
+const loadTags = () => fetch("/api/v1/tags").then(resp => resp.json()).catch(err => console.error('failed to load tags', err));
 
 const renderDocumentResultSet = (docs, containerId) => {
     console.log(docs);
@@ -38,7 +41,7 @@ const initSearch_tagify = (tagList) => {
                 <span class='tagify__tag-text'>${v}</span>
             </div>
         </tag>`,
-            "autocomplete" : true,
+            "autocomplete": true,
             "dropdown": {
                 "enabled": 3
             },
@@ -48,9 +51,9 @@ const initSearch_tagify = (tagList) => {
     );
 
     tagify.on('input', function (e) {
-         if(e.detail.length < 2 ) {
-             return;
-         }
+        if (e.detail.length < 2) {
+            return;
+        }
         tagify.settings.whitelist = tagsIfied.filter(tag => tag.value.toLowerCase().indexOf(e.detail.toLowerCase()) >= 0);
 
         if (tagify.settings.whitelist.length) {
@@ -100,13 +103,22 @@ const updateSelectOptions = (tagList, elementId) => {
     try {
         const selectize = document.getElementById(elementId).selectize;
         selectize.clear();
+        selectize.clearOptions();
         selectize.addOption(tagList);
     } catch (error) {
         console.error(error);
     }
 };
 
-const initSearch = (tagList) => {
+const refreshTagList = () => {
+    return loadTags()
+        .then(tagList => {
+            updateSelectOptions(tagList, 'tags-import');
+            updateSelectOptions(tagList, 'tags-search');
+        });
+};
+
+const initSearch = (tagList = []) => {
     const inputElement = document.getElementById('tags-search');
     const buttonSearch = document.getElementById('btn-search-by-tags');
 
@@ -119,11 +131,11 @@ const initSearch = (tagList) => {
         "labelField": 'name',
         "searchField": 'name',
         "openOnFocus": false,
-        "onChange" :  (value) =>  buttonSearch.disabled = $inputTags[0].selectize.items.length === 0,
-        "onItemAdd" : (value, $item) =>  $inputTags[0].selectize.close()
+        "onChange": (value) => buttonSearch.disabled = $inputTags[0].selectize.items.length === 0,
+        "onItemAdd": (value, $item) => $inputTags[0].selectize.close()
     });
     const tagSelectize = $inputTags[0].selectize;
-    updateSelectOptions(tagList,'tags-search');
+    updateSelectOptions(tagList, 'tags-search');
 
     // user clicks on search button
     buttonSearch.addEventListener('click', (ev) => {
@@ -144,10 +156,36 @@ const initSearch = (tagList) => {
         } catch (error) {
             console.error(error);
         }
-    });   
+    });
 };
-const initimportDocument = (tagList) => {
+/**
+ * Being given a list of value, returns an an array of object
+ * representing exisintg tags (Id property) or new tag (name property)
+ * 
+ * @param {string} tagsInput comma separated list of entries 
+ */
+const createTagsToSubmit = (tagsInput) => {
+    let hasNewTag = false;
+    const tags = tagsInput.split(',').map(tagToken => {
+        if (tagToken.indexOf(NEW_TAG_PREFIX) === -1) {
+            return {
+                "id": tagToken
+            };
+        }
+        hasNewTag = true;
+        return {
+            "name": tagToken.substr(NEW_TAG_PREFIX.length)
+        };
+    })
+    return {
+        "hasNewTag" : hasNewTag,
+        "tags" : tags 
+    }
+};
+
+const initImportDocument = (tagList = []) => {
     const inputElement = document.getElementById('tags-import');
+    const inputNameElement = document.getElementById('input-name');
     const btnImportDoc = document.getElementById('btn-import-doc');
 
     // because tagify only accepts property 'value' and not 'name'
@@ -159,45 +197,81 @@ const initimportDocument = (tagList) => {
         "labelField": 'name',
         "searchField": 'name',
         "openOnFocus": false,
-        "onChange" :  (value) =>  btnImportDoc.disabled = $inputTags[0].selectize.items.length === 0,
-        "onItemAdd" : (value, $item) =>  {
+        "onChange": (value) => btnImportDoc.disabled = $inputTags[0].selectize.items.length === 0,
+        "onItemAdd": (value, $item) => {
             console.log(value);
             $inputTags[0].selectize.close();
         },
         //"persist" : false,
-        "create": function(input) {
+        "create": function (input) {
             console.log('creating');
+            $inputTags[0].selectize.close();
             return {
-                "id": `nullid:${input}`,
+                "id": `${NEW_TAG_PREFIX}${input}`,
                 "name": input
             }
         }
     });
     const tagSelectize = $inputTags[0].selectize;
-    updateSelectOptions(tagList,'tags-import');
+    updateSelectOptions(tagList, 'tags-import');
 
     // user clicks on search button
     btnImportDoc.addEventListener('click', (ev) => {
         try {
-            const importTagIds = inputElement.value.split(',');
+            // process file content (upload) ////////////////////////////////////////
 
-            console.log(importTagIds)
+            const files = document.querySelector('[type=file]').files;
+            if (files.length === 0) {
+                console.warn('no file selected');
+                return;
+            }
 
+            const formData = new FormData();
+            let file = files[0];
+            formData.append('content', file);
+
+            // Form tags ///////////////////////////////////////////////////////////
+            
+            const {hasNewTag, tags: importTagIds} = createTagsToSubmit(inputElement.value);
+            formData.append('tags', JSON.stringify(importTagIds));
+            
+            // Form name ///////////////////////////////////////////////////////////
+            // default to filename
+
+            let docName = inputNameElement.value.trim();
+            formData.append('name', docName || file.name);
+
+            // SUbmit 
+
+            fetch("/api/v1/documents", {
+                method: 'POST',
+                body: formData
+            }).then(response => {
+                console.log(response);
+                if(hasNewTag) {
+                    refreshTagList();
+                }
+            }).catch(error => {
+                console.error(error);
+                alert('failed to import document');
+            });
 
         } catch (error) {
             console.error(error);
         }
-    });   
+    });
 };
-
-const loadTags = () => fetch("/api/v1/tags").then(resp => resp.json()).catch(err => console.error('failed to load tags', err));
 
 const start = () => {
     console.log('starting ...');
     let tagList = [];
-
+/*
     loadTags()
-        .then( (result) => tagList = result)
-        .then( () => initSearch(tagList) )
-        .then( () => initimportDocument(tagList) );
+        .then((result) => tagList = result)
+        .then(() => initSearch(tagList))
+        .then(() => initImportDocument(tagList));
+        */
+    initSearch();
+    initImportDocument();
+    refreshTagList();
 };
