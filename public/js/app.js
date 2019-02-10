db657e0f285817f4e4e1992574bacaebd9555ad6
raoul2000 <manu34@free.fr>
2019-02-10 14:28:06 +0100

    remove name from doc

--- public/js/app.js
+++ public/js/app.js
@@ -11,94 +11,10 @@ const renderDocumentResultSet = (docs, containerId) => {
     console.log(docs);
     const html = docs.map(doc => `<tr id="${doc.id}">
         <td>${doc.id}</td>
-        <td>${doc.name}</td>
         <td>${doc.content.originalName}</td>
         <td>${renderTags(doc.tags)}</td>
     </tr>`);
     document.getElementById(containerId).innerHTML = html;
-};
-
-/**
- * Initialize the document search feature
- * 
- * @param {object[]} tagList list of tags retrieved from the server
- */
-const initSearch_tagify = (tagList) => {
-
-    const inputElement = document.getElementById('tags-search');
-    const buttonSearch = document.getElementById('btn-search-by-tags');
-
-    // because tagify only accepts property 'value' and not 'name'
-    const tagsIfied = tagList.map(tag => ({ "value": tag.name, "id": tag.id }));
-
-    // init the Tagify input (see https://yaireo.github.io/tagify/)
-    const tagify = new Tagify(
-        inputElement,
-        {
-            "enforceWhitelist": true,
-            "delimiter": null,
-            "tagTemplate": (v, tagData) => `<tag title='${v}'>
-            <x title=''></x>
-            <div>
-                <span class='tagify__tag-text'>${v}</span>
-            </div>
-        </tag>`,
-            "autocomplete": true,
-            "dropdown": {
-                "enabled": 3
-            },
-            "mapValueToProp": "id",
-            "whitelist": []
-        }
-    );
-
-    tagify.on('input', function (e) {
-        if (e.detail.length < 2) {
-            return;
-        }
-        tagify.settings.whitelist = tagsIfied.filter(tag => tag.value.toLowerCase().indexOf(e.detail.toLowerCase()) >= 0);
-
-        if (tagify.settings.whitelist.length) {
-            console.log("showing");
-            //e.stopPropagation();
-            //e.preventDefault();
-            tagify.dropdown.show.call(tagify);
-            return false;
-        }
-    });
-
-    // enable search button when tag selected
-    tagify.on('add', (e) => {
-        buttonSearch.disabled = false;
-    });
-
-    // update search button state : disable when no tag selected
-    tagify.on('remove', (e) => {
-        buttonSearch.disabled = JSON.parse(inputElement.value).length === 0;
-    });
-
-    // user clicks on search button
-    buttonSearch.addEventListener('click', (ev) => {
-        try {
-            const queryTagIds = JSON.parse(inputElement.value)
-                .map(tag => tag.id)
-                .join(',');
-
-            const url = new URL('http://localhost:3000/api/v1/documents');
-            url.search = new URLSearchParams({
-                "tags": queryTagIds
-            });
-
-            fetch(url)
-                .then(resp => resp.json())
-                .then(docs => {
-                    console.log(docs);
-                    renderDocumentResultSet(docs, 'result-set');
-                });
-        } catch (error) {
-            console.error(error);
-        }
-    });
 };
 
 const updateSelectOptions = (tagList, elementId) => {
@@ -192,7 +108,6 @@ const createTagsToSubmit = (tagsInput) => {
  */
 const initImportDocument = (tagList = []) => {
     const inputElement = document.getElementById('tags-import');
-    const inputNameElement = document.getElementById('input-name');
     const btnImportDoc = document.getElementById('btn-import-doc');
 
     // because tagify only accepts property 'value' and not 'name'
@@ -241,12 +156,6 @@ const initImportDocument = (tagList = []) => {
             
             const {hasNewTag, tags: importTagIds} = createTagsToSubmit(inputElement.value);
             formData.append('tags', JSON.stringify(importTagIds));
-            
-            // Form name ///////////////////////////////////////////////////////////
-            // default to filename
-
-            let docName = inputNameElement.value.trim();
-            formData.append('name', docName || file.name);
 
             // SUbmit 
 
